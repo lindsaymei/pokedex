7a3bfc31fc232a3e54121dc5e346745c5709b1d6
carterphoenix <carterpmore@gmail.com>
2024-02-14 17:08:34 -0500

    added landing page and updates some of the styling involved

---
+++ pokemon/pokedex/src/WelcomeScreen.js
@@ -0,0 +1,16 @@
+//WelcomeScreen.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const WelcomeScreen = () => {
+  return (
+    <div className="welcome-screen">
+      <h1>Welcome to Pokemon App</h1>
+      <Link to="/home">
+        <button>Continue</button>
+      </Link>
+    </div>
+  );
+};
+
+export default WelcomeScreen;
